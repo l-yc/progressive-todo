654e0792e9f554bc27b0b8817cc659f4f6549187
l-yc <22719973+l-yc@users.noreply.github.com>
2021-05-24 22:52:27 +0800

    add track history

--- src/views/Track.vue
+++ src/views/Track.vue
@@ -5,10 +5,18 @@
       <div id="controls">
         <button @click="toggle">{{ handler === null ? "Start" : "Pause" }}</button>
         <button @click="reset">Reset</button>
+        <button @click="log">Log</button>
       </div>
       <div id="clock">
+        <h1 ref="name" contenteditable>Timer Name</h1>
         <h1>{{ timer }}</h1>
         <h1 contenteditable>Keep going!</h1>
+      </div>
+      <div id="history">
+        <div v-for="h in history" v-bind:key="h.id">
+          <span @click="erase(h.id)">&times;</span>
+          {{ h.name }}: {{ formatElapsed(h.time) }}
+        </div>
       </div>
     </div>
   </div>
@@ -16,6 +24,7 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { v4 as uuidv4 } from "uuid";
 
 export default defineComponent({
   name: "Track",
@@ -24,17 +33,28 @@ export default defineComponent({
     return {
       elapsed: 0,
       handler: null,
+      history: [],
     };
   },
   computed: {
     timer() {
-      let date = new Date(0);
-      date.setSeconds(this.elapsed); // specify value for SECONDS here
-      let timeString = date.toISOString().substr(11, 8);
-      return timeString;
+      return this.formatElapsed(this.elapsed);
     }
   },
+  created() {
+    let json = JSON.parse(localStorage.getItem("track-history") || "[]");
+    this.history = json;
+  },
   methods: {
+    formatElapsed(elapsed) {
+      let date = new Date(0);
+      date.setSeconds(elapsed); // specify value for SECONDS here
+      let timeString = date.toISOString().substr(11, 8);
+      return timeString;
+    },
+    tick() {
+      this.elapsed += 1;
+    },
     toggle() {
       if (this.handler !== null) {
         window.clearInterval(this.handler);
@@ -50,8 +70,28 @@ export default defineComponent({
       this.elapsed = 0;
       this.handler = null;
     },
-    tick() {
-      this.elapsed += 1;
+    log() {
+      this.history.unshift({
+        id: uuidv4(),
+        name: this.$refs.name.innerText,
+        time: this.elapsed,
+      });
+      this.saveData();
+    },
+    erase(id) {
+      let i = -1;
+      for (let j = 0; j < this.history.length; ++j) {
+        if (this.history[j].id == id) {
+          i = j;
+          break;
+        }
+      }
+      if (i == -1) throw "history item not found";
+      this.history.splice(i, 1);
+      this.saveData();
+    },
+    saveData() {
+      localStorage.setItem("track-history", JSON.stringify(this.history));
     },
   },
 });
